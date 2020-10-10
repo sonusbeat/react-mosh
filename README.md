2241e5dd1a37dddf9fa207183f261a83f4dfc118
qbixmex <qbixmex@gmail.com>
2020-10-09 21:24:13 -0500

    04-adding-children

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 1. Hello World
 2. Install Bootstrap
 3. Create Component
+4. Adding Children
--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,9 +3,12 @@ import React, { Component } from 'react';
 class Counter extends Component {
     render() { 
         return (
-            <h1 className="text-center bg-dark text-white py-3">
-                Counter Component
-            </h1>
+            <React.Fragment>
+                <h1 className="text-center bg-dark text-white py-3">
+                    Counter Component
+                </h1>
+                <button className="btn btn-default btn-primary">Increment</button>
+            </React.Fragment>
         );
     }
 }
