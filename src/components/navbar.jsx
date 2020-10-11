102abe11eaa4e18872367d8bd3490229b8974421
qbixmex <qbixmex@gmail.com>
2020-10-11 03:13:16 -0500

    Navbar Added

---
+++ src/components/navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const NavBar = ({totalCounters}) => {
+  return (
+    <nav className="navbar navbar-dark bg-dark mb-4">
+      <span className="navbar-brand mb-0 h1">
+        Total{" "}
+        <span className="badge badge-pill badge-secondary">
+          {totalCounters}
+        </span>
+      </span>
+    </nav>
+  );
+};
+
+export default NavBar;
